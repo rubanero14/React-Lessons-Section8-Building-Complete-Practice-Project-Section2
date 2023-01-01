ecd1c8a6978ea8a967fcddc392cdcbb91e8b6822
Rajaruban Rajindram <ahyat888@gmail.com>
2023-01-02 00:53:50 +0800

    Working with refs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
@@ -7,29 +7,25 @@ import Wrapper from "../Helpers/Wrapper";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUserName, setEnteredUserName] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState({
     title: "",
     message: "",
     error: false,
   });
 
-  // onChange event handlers
-  const userNameChangeHandler = (e) => {
-    setEnteredUserName(e.target.value);
-  };
-  const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
-  };
-
   // onSubmit event handler =>
   const addUserHandler = (event) => {
     // Prevent page from refreshing upon submit evnt fired
     event.preventDefault();
 
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
     // Gatekeeping function to bar submitting empty data
-    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       return setError({
         title: "Invalid input!",
         message: "Please fill up the inputs and dont leave them empty.",
@@ -37,7 +33,7 @@ const AddUser = (props) => {
       });
     }
 
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       return setError({
         title: "Invalid age!",
         message: "Please input valid age.",
@@ -46,11 +42,11 @@ const AddUser = (props) => {
     }
 
     // Sending new user data using props function to parent component to update new list or array of users whenever add use button is clicked
-    props.onAddUser(enteredUserName, enteredAge);
+    props.onAddUser(enteredName, enteredUserAge);
 
-    // Garbage collection section => Emptying inputs after data collected
-    setEnteredUserName("");
-    setEnteredAge("");
+    // Garbage Collection
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   // Close modal
@@ -88,19 +84,9 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            onChange={userNameChangeHandler}
-            value={enteredUserName}
-          />
+          <input type="text" id="username" ref={nameInputRef} />
           <label htmlFor="age">Age (Years)</label>
-          <input
-            type="number"
-            id="age"
-            onChange={ageChangeHandler}
-            value={enteredAge}
-          />
+          <input type="number" id="age" ref={ageInputRef} />
           <Button type="submit" style={{ marginBotttom: "10px" }}>
             Add User
           </Button>
